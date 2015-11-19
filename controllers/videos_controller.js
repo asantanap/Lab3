b0f45e36484a9d487a968b44236820bcb6a0b4f9
asantanap <asantanap.91@gmail.com>
2015-11-19 21:02:29 +0000

    version16

--- controllers/videos_controller.js
+++ controllers/videos_controller.js
@@ -4,17 +4,18 @@ exports.index = function(req, res) {
   models.Videos.findAll().then(function(listado) {
     res.render('videos.ejs', { listado: listado});
   })
-
+};
 exports.index1 = function(req, res) {
   models.Videos.findAll().then(function(listado) {
     res.render('videos1.ejs', { listado: listado});
   })
-
+};
 exports.index2 = function(req, res) {
   models.Videos.findAll().then(function(listado) {
     res.render('videos2.ejs', { listado: listado});
   })
-
 };
+
+
 
 
